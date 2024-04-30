04ab855c227a340d5ae5eb1b20f2e0accec4298e
keiff3r <keiffer@blooo.io>
2024-04-30 16:21:23 +0200

    feat: create function to add cUSD as feeCurrency

--- index.js
+++ index.js
@@ -57,6 +57,16 @@ async function signTransaction(ledger, overrides = {}, name = "eip1559") {
     console.log(`==================== END ${name} ====================\n`);
   }
 }
+async function addcUSDasFeeCurrency(ledger) {
+  console.log("Adding cUSD as fee currency");
+  let data = "0463555344765de816845861e75a25fca122bb6898b8b1282a000000120000a4ec3045022100a704051cd04a5e9f95da3abc04c0f6cbfe40c02f5b84f4361f8853fef325fc1e022056a5395b4114644450a314fc5e6f0e524b790ad39fa1907837abb6907616932f"
+  let response = await ledger.provideERC20TokenInformation(data)
+  console.log("response", response)
+  if (response !== true) {
+    throw new Error("Failed to add cUSD as fee currency");
+  }
+  console.log("cUSD added as fee currency");
+}
 
 async function main() {
   const transport = await TransportNodeHid.open("");
